d8f83bae514fad5dcc5c45c6c749aa96ed66f75d
Adhil Ameen <67848808+Aadhi13@users.noreply.github.com>
2025-03-19 09:10:08 +0530

    Add question 10

---
+++ program-10.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    char str[100], *left, *right;
+    int flag = 1;
+
+    cout << "Enter a string: ";
+    cin >> str; 
+
+    left = str;
+    right = str + strlen(str) - 1;
+
+    while(left < right) {
+        if(*left != *right){
+            flag = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if(flag == 1) {
+        cout << "The string is a palindrome.";
+    } else {
+        cout << "The string is not a palindrome.";
+    }
+
+    return 0;
+}
